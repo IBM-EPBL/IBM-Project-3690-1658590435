454a641f798c1777e3dadfac044ed13cd65bac7d
Lokeshwaran2 <71618096+Lokeshwaran2@users.noreply.github.com>
2022-10-04 10:21:39 +0530

    Create init_db.py

---
+++ Assesments/M3 Lead/Assignment 2/init_db.py
@@ -0,0 +1,30 @@
+import sqlite3
+from flask import Flask, render_template
+connection = sqlite3.connect('database.db')
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
